3a32c03b0cb38747e43a77407d6bc65eec527d4c
Forhad Roben <i.rforhad@gmail.com>
2023-03-04 02:22:19 +0600

    features,date,font-awesome icon commited

--- js/app.js
+++ js/app.js
@@ -12,16 +12,37 @@ const displayAiData = (aiTools) => {
         // console.log(singleAiTool);
         const singleAiToolDiv = document.createElement('div');
         singleAiToolDiv.classList.add('col');
+
         singleAiToolDiv.innerHTML = `
         <div class="card h-100 p-4">
-        <img style="height: 200px;" src="${singleAiTool.image}" class="card-img-top rounded" alt="...">
+            <img style="height: 200px;" src="${singleAiTool.image}" class="card-img-top rounded" alt="...">
             <div class="card-body">
-                <h5 class="card-title">${}</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <h5 class="card-title">Features</h5>
+                
+                <div>
+                    <ol>
+                        <li >${singleAiTool.features[0]}</li>
+                        <li>${singleAiTool.features[1]} </li>
+                        <li >${singleAiTool.features[2]} </li>
+                        <li >${singleAiTool.features[3]} </li>
+                    </ol>
+                </div>
+                <hr class="border border-1">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div>
+                        <h5 class="card-title">${singleAiTool.name}</h5>
+                        <p><i class="far fa-calendar-alt "></i> ${singleAiTool.published_in}</p>
+                    </div>
+                    <div class="rounded-5 px-3 py-2 bg-danger-subtle">
+                        <i class="fas fa-arrow-right"></i>
+                    </div>
+                </div>
             </div>
         </div>
         `;
+
         aiContainer.appendChild(singleAiToolDiv)
     });
 }
+
 loadData()
